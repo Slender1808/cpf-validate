cae13fabce8e6f32f84db45055767ea8753854de
Jadson G. Matos <50336793+Slender1808@users.noreply.github.com>
2020-11-12 17:43:47 -0300

    Update is-cpf.js

--- src/is-cpf.js
+++ src/is-cpf.js
@@ -1,4 +1,4 @@
-;(function (root, factory) {
+; (function (root, factory) {
   'use strict'
   /* eslint-disable no-undef */
   if (typeof define === 'function' && define.amd) {
@@ -12,48 +12,62 @@
 })(this, function () {
   'use strict'
 
-  function getVerificationDigit (value) {
-    var rest = value % 11
-    return rest < 2 ? 0 : 11 - rest
-  }
-
-  function sumDigits (first, second) {
-    return first + second
-  }
-
-  function multiplyDigits (factor) {
-    return function (digit, index) {
-      return Number(digit) * (factor - index)
+  function validateCpf(cpf) {
+    function getVerificationDigit(value) {
+      const rest = value % 11
+      return rest < 2 ? 0 : 11 - rest
     }
-  }
 
-  function isValidCpf (cpf) {
-    var isSameNumber = cpf.match(/^(\d)\1{10}$/)
-    if (isSameNumber) {
-      return false
+    function sumDigits(first, second) {
+      return first + second
     }
-    var brokenCpf = cpf.match(/^(\d{9})(\d{2})$/)
-    var withoutDigits = brokenCpf[1].split('')
-    var digits = brokenCpf[2]
 
-    var tempFirstDigit = withoutDigits
-      .map(multiplyDigits(10))
-      .reduce(sumDigits)
+    function multiplyDigits(factor) {
+      return function (digit, index) {
+        return Number(digit) * (factor - index)
+      }
+    }
 
-    var firstDigit = getVerificationDigit(tempFirstDigit)
+    const cleanCpf = String(cpf).replace(/\D/g, '')
+    if (cleanCpf.length == 11) {
+      if (() => {
+        switch (cleanCpf) {
+          case "           ":
+          case "00000000000":
+          case "11111111111":
+          case "22222222222":
+          case "33333333333":
+          case "44444444444":
+          case "55555555555":
+          case "66666666666":
+          case "77777777777":
+          case "88888888888":
+          case "99999999999":
+            console.error('cpf ' + cpf)
+            return false
+        }
+        const brokenCpf = cpf.match(/^(\d{9})(\d{2})$/)
+        const withoutDigits = brokenCpf[1].split('')
+        const digits = brokenCpf[2]
 
-    var tempSecondDigit = withoutDigits.concat(firstDigit)
-      .map(multiplyDigits(11))
-      .reduce(sumDigits)
+        let tempFirstDigit = withoutDigits
+          .map(multiplyDigits(10))
+          .reduce(sumDigits)
 
-    var secondDigit = getVerificationDigit(tempSecondDigit)
+        const firstDigit = getVerificationDigit(tempFirstDigit)
 
-    return (String(firstDigit) + secondDigit) === digits
-  }
+        let tempSecondDigit = withoutDigits.concat(firstDigit)
+          .map(multiplyDigits(11))
+          .reduce(sumDigits)
 
-  function validateCpf (cpf) {
-    var cleanCpf = String(cpf).replace(/\D/g, '')
-    return cleanCpf.length === 11 && isValidCpf(cleanCpf)
+        const secondDigit = getVerificationDigit(tempSecondDigit)
+
+        return (String(firstDigit) + secondDigit) === digits
+      }) {
+        return true
+      }
+    }
+    return false
   }
 
   return validateCpf
